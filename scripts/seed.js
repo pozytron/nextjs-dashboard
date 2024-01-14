8cd114fcbe889c66e2008ecb13347e303c1fa25d
pozytron <michal.tymula@gmail.com>
2024-01-14 19:38:15 +0100

    feat: add db and seed script

--- scripts/seed.js
+++ scripts/seed.js
@@ -1,6 +1,12 @@
-import {db} from '@vercel/postgres';
-import {invoices, customers, revenue, users} from '@/app/lib/placeholder-data';
-import {hash} from 'bcrypt';
+// this should be CommonJS style
+const { db } = require('@vercel/postgres');
+const {
+  invoices,
+  customers,
+  revenue,
+  users,
+} = require('../app/lib/placeholder-data.js');
+const bcrypt = require('bcrypt');
 
 async function seedUsers(client) {
   try {
@@ -19,8 +25,8 @@ async function seedUsers(client) {
 
     // Insert data into the "users" table
     const insertedUsers = await Promise.all(
-        users.map(async (user) => {
-          const hashedPassword = await hash(user.password, 10);
+      users.map(async (user) => {
+        const hashedPassword = await bcrypt.hash(user.password, 10);
         return client.sql`
         INSERT INTO users (id, name, email, password)
         VALUES (${user.id}, ${user.name}, ${user.email}, ${hashedPassword})
